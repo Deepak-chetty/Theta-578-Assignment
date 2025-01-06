cdaa89284d0dd8ac4ac43694efc37429075e9d60
Deepak-chetty <149146582+Deepak-chetty@users.noreply.github.com>
2025-01-06 21:04:45 +0530

    Create basic.py
    Made it for submitting as ExcelR Assignment.

---
+++ basic.py
@@ -0,0 +1,60 @@
+# problem - 1
+
+str1 = "Hello"
+str2 = "World"
+result = str1 + " ," + str2 
+print(result)
+
+# problem - 2
+
+str3 = "Python" 
+result = str3 * 3
+print(result)
+
+# problem - 3
+
+str4 = "Hello  Team!" 
+length = len(str4)
+print(length) 
+
+# problem - 4
+
+str5 = "Sub String Test" 
+substring = str5[ 3:6]
+print(substring) 
+
+# problem - 5
+
+str6 = "  Beautiful  "
+result = str6.strip()
+print(result)  
+
+# problem - 6
+
+str7 = "work hard"
+result = str7.replace("hard", "smart")
+print(result)
+
+# problem - 7
+
+str1 = "apple,banana,orange"
+result = str1.split(",")
+print(result)  
+
+# problem - 8
+
+str1 = "Hello World"
+index = str1.find("World")
+print(index)  
+
+# problem - 9
+
+words = ["Hello", "welcome", "AIML","batch"]
+result = " ".join(words)
+print(result)
+
+# problem - 10
+
+str1 = "Test for slice of string" 
+char = str1[3]
+print(char)
