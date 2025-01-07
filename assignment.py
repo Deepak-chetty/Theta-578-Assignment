f00b049d4c2bfa24c48504739ca3d86aedaeb449
Deepak-chetty <149146582+Deepak-chetty@users.noreply.github.com>
2025-01-07 13:13:58 +0530

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -8,3 +8,18 @@ students = {
 
 for student_id, detailes in students.items():
     print(f"Student-ID: {student_id} & Name: {detailes["name"]} ; age: {detailes["age"]} - Marks is {detailes["marks"]}")
+
+import pandas as pd
+data = [20,30,40,50,2,3,6,8,10,19,20,24,2,4,5,6]
+
+No_of_studied=data[4:8]
+print("No.Of hours_studied",No_of_studied)
+
+Age = data[9:13]
+print("Student_age",Age)
+
+screen_time=data[12:18]
+print("screen_time",screen_time)
+
+Data_Frame=pd.DataFrame(data)
+print(Data_Frame)
